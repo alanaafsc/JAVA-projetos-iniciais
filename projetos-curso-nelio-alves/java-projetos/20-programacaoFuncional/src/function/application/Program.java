00bec7b815444b99ddb782de880511eb0b78c5bd
alanaafsc <alana.ingrid@ufpe.br>
2022-10-18 15:32:27 -0300

    Function - expressão lambda inline

--- projetos-curso-nelio-alves/java-projetos/20-programacaoFuncional/src/function/application/Program.java
+++ projetos-curso-nelio-alves/java-projetos/20-programacaoFuncional/src/function/application/Program.java
@@ -3,7 +3,6 @@ package function.application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import function.entities.Product;
@@ -19,9 +18,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		Function<Product, String> func = p -> p.getName().toUpperCase();
+		//Function<Product, String> func = p -> p.getName().toUpperCase();
 		
-		List<String> names = list.stream().map(func).collect(Collectors.toList());
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 		names.forEach(System.out::println);
 	}
 }
